d09d91a32feb5861c6aafcfbe8bb394e97b39265
Milan Barot <7milan16@gmail.com>
2019-07-17 20:55:04 -0400

    initial commit

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/1f5da952-db12-44b3-a282-4ca38912fdf4/token";
+const instanceLocator = "v1:us1:1f5da952-db12-44b3-a282-4ca38912fdf4";
+
+export { tokenUrl, instanceLocator }
